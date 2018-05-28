2c9772010f506a22f9b704aa26b55c51c57061df
Bruno Contreras <bcontreras@ets.com>
2018-05-29 01:31:23 +0200

    Lectura de las presentaciones

--- src/renderer/logic.js
+++ src/renderer/logic.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 
-const MAIN_DIRECTORIES = {
+const ROOT_DIRECTORIES = {
   EXPERTS: 'Expertos',
   FAMILY: 'Familia',
   TRAINING: 'Formación',
@@ -14,14 +14,48 @@ const MAIN_DIRECTORIES = {
 //   VIDEOS: 'Vídeos'
 // }
 
-const result = {
+const data = {
   experts: [],
   family: [],
   training: [],
-  conferences: []
+  conferences: [],
+  presentations: {},
+  videos: {},
+  audios: {}
 }
 
 const log = []
+
+let presentationIdx = 0
+// let videoIdx = 0
+// let audioIdx = 0
+
+const newPresentation = ({ path, name }) => {
+  presentationIdx++
+  const id = presentationIdx.toString()
+  const presentation = { id, path, name }
+  data.presentations[id] = presentation
+  return presentation
+}
+
+// const newAudio = ({ path, name }) => {
+
+// }
+
+// const newVideo = ({ path, name }) => {
+
+// }
+
+// const readAudioDir = path => {
+
+// }
+
+const readSlidesDir = path => {
+  const files = fs.readdirSync(path)
+  if (files) {
+    return files.sort()
+  }
+}
 
 const readPresentation = ({ path, name, breadCrumb }) => {
   const files = fs.readdirSync(path)
@@ -29,7 +63,21 @@ const readPresentation = ({ path, name, breadCrumb }) => {
     log.push(`La carpeta ${breadCrumb.join(' > ')} no tiene contenido`)
     return []
   } else {
-    return []
+    const presentation = newPresentation({ path, name })
+    files.forEach(file => {
+      switch (file) {
+        case 'Audios':
+          // presentation.audios = readAudioDir(path)
+          break
+        case 'Videos':
+          // presentation.videos = readVideoDir(path)
+          break
+        case 'PowerPoint':
+          presentation.slides = readSlidesDir(`${path}\\${file}`)
+          break
+      }
+    })
+    return presentation.id
   }
 }
 
@@ -40,15 +88,18 @@ const readDir = ({ path, name, breadCrumb, isContainer = true }) => {
     return []
   } else {
     if (isContainer) {
-      return files.map(file => readDir({
-        path: `${path}/${file}`,
+      return files.map(file => ({
         name: file,
-        breadCrumb: [...breadCrumb, file],
-        isContainer: false
+        presentations: readDir({
+          path: `${path}\\${file}`,
+          name: file,
+          breadCrumb: [...breadCrumb, file],
+          isContainer: false
+        })
       }))
     } else {
       return files.map(file => readPresentation({
-        path: `${path}/${file}`,
+        path: `${path}\\${file}`,
         name: file,
         breadCrumb: [...breadCrumb, file]
       }))
@@ -56,36 +107,37 @@ const readDir = ({ path, name, breadCrumb, isContainer = true }) => {
   }
 }
 
-const checkResult = () => {
-  if (result.experts.length === 0) log.push(`No existe la carpeta '${MAIN_DIRECTORIES.EXPERTS}'`)
-  if (result.family.length === 0) log.push(`No existe la carpeta '${MAIN_DIRECTORIES.FAMILY}'`)
-  if (result.training.length === 0) log.push(`No existe la carpeta '${MAIN_DIRECTORIES.TRAINING}'`)
-  if (result.conferences.length === 0) log.push(`No existe la carpeta '${MAIN_DIRECTORIES.CONFERENCES}'`)
+const checkData = () => {
+  if (data.experts.length === 0) log.push(`No existe la carpeta '${ROOT_DIRECTORIES.EXPERTS}'`)
+  if (data.family.length === 0) log.push(`No existe la carpeta '${ROOT_DIRECTORIES.FAMILY}'`)
+  if (data.training.length === 0) log.push(`No existe la carpeta '${ROOT_DIRECTORIES.TRAINING}'`)
+  if (data.conferences.length === 0) log.push(`No existe la carpeta '${ROOT_DIRECTORIES.CONFERENCES}'`)
 }
 
 const readRootDirectory = path => {
+  debugger
   const files = fs.readdirSync(path)
   if (!files) log.push('La carpeta raíz no tiene contenido')
   else {
     files.forEach(file => {
-      const filePath = `${path}/${file}`
+      const filePath = `${path}\\${file}`
       const breadCrumb = [file]
       switch (file) {
-        case MAIN_DIRECTORIES.EXPERTS:
-          result.experts = readDir({ path: filePath, name: file, breadCrumb })
+        case ROOT_DIRECTORIES.EXPERTS:
+          data.experts = readDir({ path: filePath, name: file, breadCrumb })
           break
-        case MAIN_DIRECTORIES.FAMILY:
-          result.family = readDir({ path: filePath, name: file, breadCrumb })
+        case ROOT_DIRECTORIES.FAMILY:
+          data.family = readDir({ path: filePath, name: file, breadCrumb })
           break
-        case MAIN_DIRECTORIES.TRAINING:
-          result.training = readDir({ path: filePath, name: file, breadCrumb })
+        case ROOT_DIRECTORIES.TRAINING:
+          data.training = readDir({ path: filePath, name: file, breadCrumb })
           break
-        case MAIN_DIRECTORIES.CONFERENCES:
-          result.conferences = readDir({ path: filePath, name: file, breadCrumb })
+        case ROOT_DIRECTORIES.CONFERENCES:
+          data.conferences = readDir({ path: filePath, name: file, breadCrumb, isContainer: false })
           break
       }
     })
-    checkResult()
+    checkData()
   }
 }
 
