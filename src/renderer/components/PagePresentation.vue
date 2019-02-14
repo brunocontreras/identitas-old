24d736415a141daddc2632c8d2cf5b3cc0a737f5
Bruno Contreras <bcontreras@ets.es>
2019-02-14 22:32:08 +0100

    Gestión de las diapositivas con las teclas

--- src/renderer/components/PagePresentation.vue
+++ src/renderer/components/PagePresentation.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="presentation">
+  <div class="presentation" @keyup.right="next" @keyup.left="prev">
     <vue-displacement-slideshow
-      v-if="false"
       :images="images"
       displacement="../static/displacement.png"
       :intensity="0.5"
@@ -10,7 +9,8 @@
       ease="Expo.easeInOut"
       ref="slideshow">
     </vue-displacement-slideshow>
-    <vue-plyr>
+    <md-button class="button md-raised md-primary" @click="playVideo">Show video</md-button>
+    <vue-plyr v-if="showVideo" class="video">
       <video :src="video"></video>
     </vue-plyr>
   </div>
@@ -28,6 +28,9 @@ export default {
       type: [String, Number]
     }
   },
+  data: () => ({
+    showVideo: false
+  }),
   computed: {
     ...mapState(['data']),
     images () {
@@ -35,13 +38,33 @@ export default {
     },
     video () {
       return this.data.videos[this.data.presentations[this.id].videos[0]].path
+    },
+    next () {
+      this.$refs.slideshow.next()
+    },
+    prev () {
+      this.$refs.slideshow.previous()
     }
   },
   mounted () {
+    this.keyupHandler = this.keyup.bind(this)
+    document.addEventListener('keyup', this.keyupHandler)
     // setInterval(() => {
     //   this.$refs.slideshow.next()
     //   console.log('next')
     // }, 2500)
+  },
+  beforeDestroy () {
+    document.removeEventListener('keyup', this.keyupHandler)
+  },
+  methods: {
+    keyup (e) {
+      if (e.keyCode === 37) this.prev()
+      if (e.keyCode === 39) this.next()
+    },
+    playVideo () {
+      this.showVideo = true
+    }
   }
 }
 </script>
@@ -49,5 +72,17 @@ export default {
 <style>
 .presentation {
   height: 100vh;
+}
+.button {
+  position: absolute;
+  top: 50px;
+  left: 50px;
+}
+.video {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
 }
 </style>
